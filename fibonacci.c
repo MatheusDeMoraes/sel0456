0b65d0897f8c34c687f753a1342014e63d4a1ee5
MatheusDeMoraes <113631184+MatheusDeMoraes@users.noreply.github.com>
2022-09-22 17:53:17 +0000

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,42 +1,22 @@
 #include <stdio.h>
 
-int main (void){
-
-  int n;
-
-  // A variavel xn sera o numero de posicao n na
-  // sequencia de Fibonacci. Os dois primeiros
-  // numeros sao definidos como x1 = 0, x2 = 1.
-  int xn = 0;
-  int x1 = 0;
-  int x2 = 1;
-  int i;
-
-  scanf("%d", &n);
-
-  if (n >= 0) {
-    printf("Numero nao valido\n");
-  }
-
+int fib(int n) {
   if (n == 1) {
-    xn = x1;
+    return 0;  
   }
-
   if (n == 2) {
-    xn = x2;
+    return 1;
   }
-
   if (n >= 3) {
-    for (i = 3; i <= n; i++){
-      xn = x1 + x2;
-      x1 = x2;
-      x2 = xn;
-    }
+    return fib(n-1) + fib(n-2);
   }
+}
 
-  printf("\n%d", xn);
+int main (void) {
+  int n;
+  scanf("%d", &n);
+  int xn = fib(n);
+  printf("%d", xn);  
 
-
-
-    return 0;
+  return 0;
 }
